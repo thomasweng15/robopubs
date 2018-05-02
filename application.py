c367166f5cb95cef6a482d8132b77a6237f1ea54
Thomas Weng <thomas.weng11@gmail.com>
2018-05-02 18:27:51 -0400

    Serve in-memory data

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
+data = [
+	{"abstract": "abstract1", "title": "title1"}
+]
+
 @app.route("/")
-def hello():
-	return "<h1>Hello there</h2>"
+def index():
+	return jsonify(data)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
